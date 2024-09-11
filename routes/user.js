61228fe46e31f1850e9a94714982200b9da9a637
Rashika Poswal <rashikaposwal28@gmail.com>
2024-09-11 14:24:56 +0530

    updation .....

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const usercontroller = require('../controllers/usercontroller');
+const router = express.Router();
+router.use(express.json());
+
+router.get('/users',(req, res)=>{
+    usercontroller.getAllUsers(req,res)
+})
+
+router.get('/user/:id',(req, res)=>{
+    usercontroller.getParticularUser(req,res)
+})
+router.post('/add/user',(req,res)=>{
+    usercontroller.addUser(req, res)
+})
+module.exports = router;
